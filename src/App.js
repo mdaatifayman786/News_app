04e98c1960c75becc7f36ab41b1228711a693734
mdaatifayman786 <mdaatifayman786@gmail.com>
2024-03-30 12:42:36 +0530

    news apps start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HI this react class based coponenet
+        </h1>
+      </div>
+    )
+  }
+}
+
