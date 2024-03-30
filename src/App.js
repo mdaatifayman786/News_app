a764ced0fcb1dec3f4df64212689158654dcc2fe
mdaatifayman786 <mdaatifayman786@gmail.com>
2024-03-30 14:01:21 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import NavBar from "./components/NavBar"
 
 import React, { Component } from 'react'
 
@@ -6,8 +7,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>HI this react class based coponenet
-        </h1>
+        <NavBar />
       </div>
     )
   }
